8c404935288bdc890c781ad3600762dd8deaac20
Nicholas Curl <11838540+powerlego@users.noreply.github.com>
2022-12-22 17:45:51 -0600

    added selenium instantiation

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import time
 
 from selenium import webdriver
+from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.common.by import By
 
 
 def main():
@@ -14,7 +16,15 @@ def main():
         "report_automation",
         "msedgedriver.exe",
     )
-    print("Hello World!")
+    service = EdgeService(driver_location)
+    driver = webdriver.Edge(service=service)
+    driver.get("https://www.google.com")
+    time.sleep(5)
+    search_box = driver.find_element(By.NAME, "q")
+    search_box.send_keys("EdgeDriver")
+    search_box.submit()
+    time.sleep(5)  # Let the user actually see something!
+    driver.quit()
 
 
 if __name__ == "__main__":
