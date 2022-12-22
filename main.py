c979e03bb41d85e251e32ee0288025235e15598d
Nicholas Curl <11838540+powerlego@users.noreply.github.com>
2022-12-22 17:35:46 -0600

    updated gitignore and settings

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ import time
 
 from selenium import webdriver
 
+
 def main():
-    USERPROFILE = os.environ['USERPROFILE']
-    driver_location = os.path.join(USERPROFILE, 'AppData', 'Local','Programs','report_automation', 'msedgedriver.exe')
-    print('Hello World!')
-    
-    
-if __name__ == '__main__':
+    USERPROFILE = os.environ["USERPROFILE"]
+    driver_location = os.path.join(
+        USERPROFILE,
+        "AppData",
+        "Local",
+        "Programs",
+        "report_automation",
+        "msedgedriver.exe",
+    )
+    print("Hello World!")
+
+
+if __name__ == "__main__":
     main()
-    
